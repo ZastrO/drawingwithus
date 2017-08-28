81e7bb22e2b05571c0d4a092daaca4a8a21ffb98
ZastrO <z.a.standridge@gmail.com>
2017-08-28 14:58:48 -0700

    Port Change
    Changed port to 4027

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ io.on('connection', function(socket){
 	});
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 4027;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 http.listen(server_port, server_ip_address, function(){
 	console.log('server active | listening on ' +server_ip_address+ ':' +server_port);
