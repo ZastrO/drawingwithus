fbd49c58d99cdeec58b844ee9e12fb3540f1a44f
David Baer <david@Border7.com>
2017-08-29 23:36:21 -0700

    Merged user.route.js
    Resolved conflict

--- user.route.js
+++ user.route.js
@@ -12,31 +12,32 @@ module.exports = function(req, res){
 			if(err){	
 				res.status(500);
 				res.render('error', {code: 500, msg: 'There was an unknown server error. :('});
+			} else {
+				req.session.defaults = {thickness: thickness, color: color};
+				return res.redirect('/u/'+resName);
 			}
 		}); // success
-
 		console.log( res.locals.username, color, thickness, bio, avatar );
-	}
-
-	User.findOne({username:reqName}).exec(function(err, doc){ 
-		if(err){
-			res.status(500);
-			res.render('error', {code: 500, msg: 'There was an error with your request!'});
-		} else {
-			if(doc == null){
-				res.status(404);
-				res.render('error', {code: 404, msg: 'There is no user with that name!'});
+	} else {
+		User.findOne({username:reqName}).exec(function(err, doc){ 
+			if(err){
+				res.status(500);
+				res.render('error', {code: 500, msg: 'There was an error with your request!'});
 			} else {
-				var rooms = [];	
-				Room.find({id: doc.myRooms}).exec(function(err, rDocs){
-					if(rDocs) {
-						res.render('user', {user: doc, rooms: rDocs} );	
-					} else {
-						res.render('user', {user: doc} );
-					}
-				});
+				if(doc == null){
+					res.status(404);
+					res.render('error', {code: 404, msg: 'There is no user with that name!'});
+				} else {
+					var rooms = [];	
+					Room.find({id: doc.myRooms}).exec(function(err, rDocs){
+						if(rDocs) {
+							res.render('user', {user: doc, rooms: rDocs} );	
+						} else {
+							res.render('user', {user: doc} );
+						}
+					});
+				}
 			}
-		}
-	});
-
+		});
+	}
 }
